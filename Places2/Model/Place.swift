8a2c8c6459a4eaca5187abdc57c5caacc44b122b
Rwoltx <richardx1016@gmail.com>
2018-09-06 13:09:29 -0400

    Created Place object

---
+++ Places2/Model/Place.swift
@@ -0,0 +1,24 @@
+//
+//  Place.swift
+//  Places2
+//
+//  Created by Rick Wolter on 9/6/18.
+//  Copyright © 2018 RNWolter. All rights reserved.
+//
+
+import Foundation
+
+
+struct Place {
+    
+    let name: String
+    let location: (latitude: Double, longitude: Double)
+    
+    init(name: String, latitude: Double, longitude: Double)
+    
+    
+    self.name = name
+    self.location = (latitude: latitude, longitude: longitude)
+
+    
+}
